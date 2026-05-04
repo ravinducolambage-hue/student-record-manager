e816403ae760f524f7ec65550b52158e536e844e
ravinducolambage-hue <ravinducolambage@gmail.com>
2026-05-04 14:02:06 +0530

    Implemented viewStudents feature

--- Main.java
+++ Main.java
@@ -9,11 +9,20 @@ public class Main {
         addStudent("Alice");
         addStudent("Bob");
 
+        viewStudents();
+
     }
 
     static void addStudent(String name) {
         students.add(name);
         System.out.println(name + " added.");
+    }
+
+    static void viewStudents() {
+        System.out.println("Student List:");
+        for (String s : students) {
+            System.out.println(s);
+        }
     }
 
 }
